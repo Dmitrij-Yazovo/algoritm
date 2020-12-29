6b5e329368181ff2bcfd6aec7f0ec19b4f1e0340
Dmitrij-Yazovo <vbghend@gmail.com>
2020-12-29 11:40:37 +0900

    1229

--- algorithm/src/a201228/A1546.java
+++ algorithm/src/a201228/A1546.java
@@ -1,5 +1,32 @@
 package a201228;
 
-public class A1546 {
+import java.util.Scanner;
 
+public class A1546 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int[] a = new int[n];
+		int max = 0;
+		int total = 0;
+		
+		
+		for(int i=0;i<a.length;i++) {
+			a[i] = s.nextInt();
+			if(a[i]>max)
+				max = a[i];
+		}
+		
+		
+		int[] aNew = new int[n];
+		for(int i = 0; i<aNew.length;i++) {
+			aNew[i] = (a[i]/max)*100;
+		}
+
+
+		double avg = (double)total/n;
+		System.out.println(avg);
+		
+		
+	}
 }
